4844e105b558e9d5868a8e19ce6d542c03453958
Bruno F. Cardoso <brunofranciscus@hotmail.com>
2018-03-03 20:31:50 -0300

    Fixing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="node_modules/bootstrap/complier/bootstrap.css">
-    <link rel="stylesheet" href="node_modules/bootstrap/complier/style.scss">
+    <link rel="stylesheet" href="node_modules/bootstrap/complier/style.css">
     <link rel="stylesheet" href="node_modules/bootstrap/complier/css/font-awesome.min.css">
     <title></title>
 <script type="text/javascript" src="js/jquery.js"></script>
